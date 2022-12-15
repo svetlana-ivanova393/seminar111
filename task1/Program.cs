6e170edd23423fea2a2985b52cb3fdb34ee5705e
Svetlana_Ivanova <nika.svetlanka@mail.ru>
2022-12-15 22:51:41 +0300

    task 1, 3, 5, 7

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+// a = 25, b = 5 -> да 
+// a = 2, b = 10 -> нет 
+// a = 9, b = -3 -> да 
+// a = -3 b = 9 -> нет
+
+Console.Write("Введите целое число №1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число №1: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b * b)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
---
+++ task3/Program.cs
@@ -0,0 +1,58 @@
+﻿// Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда 
+// 5 -> Пятница
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a <= 0 || a >= 8)
+{
+    Console.Write("Такого дня не существует");
+}
+else
+{
+    if (a == 1) Console.Write("Понедельник");
+    if (a == 2) Console.Write("Вторник");
+    if (a == 3) Console.Write("Среда");
+    if (a == 4) Console.Write("Четверг");
+    if (a == 5) Console.Write("Пятница");
+    if (a == 6) Console.Write("Суббота");
+    if (a == 7) Console.Write("Воскресенье");
+}
+
+// Switch кейс
+//
+// int DayNumber = Convert.ToInt32(Console.ReadLine());
+// switch (DayNumber)
+// {
+//     case 1:
+// /       Console.Write("Введите число: ");
+
+//         int a = Convert.ToInt32(Console.ReadLine()); // Convert.ToInt32 - конвертируем в нужный нам тип - при вводе данных из консоли
+//         int square = a * a;
+//         Console.WriteLine("Квадрат числа равен " + square);
+//         break;
+//     case 2:
+//         Console.WriteLine("Вторник");
+//         break;
+//     case 3:
+//         Console.WriteLine("Среда");
+//         break;
+//     case 4:
+//         Console.WriteLine("Четверг");
+//         break;
+//     case 5:
+//         Console.WriteLine("Пятница");
+//         break;
+//     case 6:
+//         Console.WriteLine("Субота");
+//         break;
+//     case 7:
+//         Console.WriteLine("Воскресенье");
+//         break;
+
+//     default:
+//         Console.WriteLine("Такого дня не существует");
+//         break;
+// }
