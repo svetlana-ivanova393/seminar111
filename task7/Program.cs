e2e2013cfb0ba7b9befc8ada4f0fd07e33de9988
Svetlana_Ivanova <nika.svetlanka@mail.ru>
2022-12-15 22:59:24 +0300

    task 5,7

---
+++ task7/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на входтрёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(a % 10);
+
+// или
+
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// if (a >= 100 && a < 1000)
+// {
+// Console.Write("Последняя цифра:");
+// Console.Write (a % 10);
+// }
+// else 
+// {
+//     Console.Write("Введено не 3-хзначное число");
+// }
